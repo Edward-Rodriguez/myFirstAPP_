dc0485bd6eb3219831728515f489ce22d9ca567c
Edward Rodriguez <Eddie@Users-MacBook-Air.local>
2018-04-03 16:53:19 -0400

    Initial Commit

---
+++ Hi_I_Am_Edward/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hi_I_Am_Edward
+//
+//  Created by Edward Rodriguez on 4/3/18.
+//  Copyright © 2018 Edward Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
